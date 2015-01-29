f61b6f01616a5bcd08f1631bcbd9f8ea3a93ce39
Daniel Kessler <sinani201@gmail.com>
2015-01-29 15:02:39 -0800

    added jan 29 to b days

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ window.onload = function() {
 	var now = Date.now();
 	var adates = [new Date(2015, 1, 30), new Date(2015, 2, 2), new Date(2015, 2, 4), new Date(2015, 2, 9), new Date(2015, 2, 11), new Date(2015, 2, 13), new Date(2015, 2, 18), new Date(2015, 2, 23), new Date(2015, 2, 25), new Date(2015, 3, 2), new Date(2015, 3, 4), new Date(2015, 3, 6), new Date(2015, 3, 11), new Date(2015, 3, 16), new Date(2015, 3, 18), new Date(2015, 3, 20), new Date(2015, 3, 23), new Date(2015, 3, 25)];
 
-	var bdates = [new Date(2015, 2, 3), new Date(2015, 2, 5), new Date(2015, 2, 6), new Date(2015, 2, 10), new Date(2015, 2, 12), new Date(2015, 2, 17), new Date(2015, 2, 19), new Date(2015, 2, 24), new Date(2015, 2, 26), new Date(2015, 2, 27), new Date(2015, 3, 3), new Date(2015, 3, 5), new Date(2015, 3, 10), new Date(2015, 3, 12), new Date(2015, 3, 13), new Date(2015, 3, 17), new Date(2015, 3, 19), new Date(2015, 3, 24), new Date(2015, 3, 26), new Date(2015, 3, 27)];
+	var bdates = [new Date(2015, 1, 29), new Date(2015, 2, 3), new Date(2015, 2, 5), new Date(2015, 2, 6), new Date(2015, 2, 10), new Date(2015, 2, 12), new Date(2015, 2, 17), new Date(2015, 2, 19), new Date(2015, 2, 24), new Date(2015, 2, 26), new Date(2015, 2, 27), new Date(2015, 3, 3), new Date(2015, 3, 5), new Date(2015, 3, 10), new Date(2015, 3, 12), new Date(2015, 3, 13), new Date(2015, 3, 17), new Date(2015, 3, 19), new Date(2015, 3, 24), new Date(2015, 3, 26), new Date(2015, 3, 27)];
 
 	var adatesap = [new Date(2015, 4, 13), new Date(2015, 4, 15), new Date(2015, 4, 17), new Date(2015, 4, 20), new Date(2015, 4, 22), new Date(2015, 4, 27), new Date(2015, 4, 29), new Date(2015, 5, 1), new Date(2015, 5, 4), new Date(2015, 5, 6)];
 
